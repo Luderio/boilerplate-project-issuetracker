540e3d9201ab8ee1b6c666dec0a4cdc226eb1db8
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-19 16:09:12 +0800

    FUnctional test test 3 updated

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,7 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 let id1 = '';
+let id2 = '';
 
 suite('Functional Tests', function() {
     suite('API Route Tests', function() {
@@ -40,7 +41,7 @@ suite('Functional Tests', function() {
                 chai.request(server)
                 .post('/api/issues/apitest')
                 .send({
-                    "issue_title": 'Test Issue',
+                    "issue_title": 'Test Issue 2',
                     "issue_text": 'Functional tests - Every field filled in',
                     "created_by": 'Luderio Sanchez',
                     "assigned_to": '',
@@ -48,13 +49,13 @@ suite('Functional Tests', function() {
                 })
                 .end(function(error, response) {
                     assert.equal(response.status, 200);
-                    assert.equal(response.body.issue_title, 'Test Issue');
+                    assert.equal(response.body.issue_title, 'Test Issue 2');
                     assert.equal(response.body.issue_text, 'Functional tests - Every field filled in');
                     assert.equal(response.body.created_by, 'Luderio Sanchez');
                     assert.equal(response.body.assigned_to, '');
                     assert.equal(response.body.status_text, '');
-                    id1 = response.body._id;
-                    console.log('id 1 has been set as: ' + id1);
+                    id2 = response.body._id;
+                    console.log('id 1 has been set as: ' + id2);
                     done();
                 });
             });
@@ -64,21 +65,11 @@ suite('Functional Tests', function() {
                 chai.request(server)
                 .post('/api/issues/apitest')
                 .send({
-                    "issue_title": '',
-                    "issue_text": '',
-                    "created_by": '',
-                    "assigned_to": 'Ian Rusiana',
-                    "status_text": 'In Progress'
+                    "issue_title": 'Test Issue 2',
+                    
                 })
                 .end(function(error, response) {
-                    assert.equal(response.status, 200);
-                    assert.equal(response.body.issue_title, undefined);
-                    assert.equal(response.body.issue_text, undefined);
-                    assert.equal(response.body.created_by, undefined);
-                    assert.equal(response.body.assigned_to, 'Ian Rusiana');
-                    assert.equal(response.body.status_text, 'In Progress');
-                    id1 = response.body._id;
-                    console.log('id 1 has been set as: ' + id1);
+                    assert.equal(response.body.error, 'required field(s) missing');
                     done();
                 });
             });
