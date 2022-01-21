c061f14fc99a06f38b3ba178febae6d9b54f5c86
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-21 11:38:15 +0800

    api.js updated

--- routes/api.js
+++ routes/api.js
@@ -29,20 +29,13 @@ module.exports = function (app) {
   
     .get(function (req, res){
       let project = req.params.project;
+      let objectFilter = Object.assign(req.query);//collects the search queries from the browser.
+      objectFilter['project'] = project;
 
-      let _id = req.query._id;
-      let issue_title = req.query.issue_title;
-      let issue_text = req.query.issue_text;
-      let created_by = req.query.created_by;
-      let assigned_to = req.query.assigned_to;
-      let status_text = req.query.status_text;
-      let open = req.query.open;
-      let created_on = req.query.created_on;
-      let updated_on = req.query.updated_on;
-
+      console.log(objectFilter)
 
       //to dispplay all issue records on '/api/issues//api/issues/apitest/
-      IssueLogs.find({"project": project},
+      IssueLogs.find(objectFilter,
       (error, issueRecords) => {
         if (error) return console.log(error);
 
@@ -78,64 +71,8 @@ module.exports = function (app) {
 
         //outputs all issue logs from apitest.
         responseObject = logResult;
-
-        if (_id || issue_title || issue_text || created_on || updated_on || created_by || assigned_to || open || status_text) {
-
-          //stores the search queries from the search bar of the browser. 
-          let searchQuery = {};
-
-          if (_id) {searchQuery['_id'] = _id;}
-
-          if (issue_title) {searchQuery['issue_title'] = issue_title;}
-
-          if (issue_text) {searchQuery['issue_text'] = issue_text;}
-
-          if (created_on) {searchQuery['created_on'] = created_on;}
-
-          if (updated_on) {searchQuery['updated_on'] = updated_on;}
-
-          if (created_by) {searchQuery['created_by'] = created_by;}
-
-          if (assigned_to) {searchQuery['assigned_to'] = assigned_to;}
-
-          if (open) {
-            //condition to handle the ?open=true or ?open=false values from string to boolean value. 
-              if (open == "true") {
-                open = true;
-                }else if (open == "false") {
-                open = false;
-                }
-                searchQuery['open'] = open;
-              }
-
-          if (status_text) {searchQuery['status_text'] = status_text;}
-
-          //extrtacts the serach queries by key and value.
-          let searchKeys = Object.keys(searchQuery);
-          let searchValues = Object.values(searchQuery);
-
-          let searchKeysItems = searchKeys.map(keys => {
-            let searchValuesItems = searchValues.map(values => {
-              let logSearch = logResult.map(logs => {
-                for (keys in logs) {
-                  if (logs[keys] == values) {
-                    console.log(logs[keys] == values);
-                  }
-                }
-              });
-            });
-          });
-
-          //console.log(logSearch);
-
-        
-        }
-
         res.json(responseObject);
       });
-
-
-
     })
     
     .post(function (req, res){
