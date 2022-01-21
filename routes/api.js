00365c2e926c7d9c686565fc920e79d9f0fc8f1e
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2022-01-21 14:24:28 +0800

    api.js

--- routes/api.js
+++ routes/api.js
@@ -114,9 +114,6 @@ module.exports = function (app) {
     .put(function (req, res){
       let project = req.params.project;
       
-      let id = req.body._id || res.json({"error": "missing _id"});
-      
-
       let newDetails = Object.assign(req.body);
       newDetails['updated_on'] = new Date().toISOString();
 
